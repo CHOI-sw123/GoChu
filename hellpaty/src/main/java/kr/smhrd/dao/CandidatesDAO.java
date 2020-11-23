95713a40ed0e78302ff91585c024c9a814f7ad75
gkstjd2605 <68883700+gkstjd2605@users.noreply.github.com>
2020-11-23 11:54:46 +0900

    Update CandidatesDAO.java

--- hellpaty/src/main/java/kr/smhrd/dao/CandidatesDAO.java
+++ hellpaty/src/main/java/kr/smhrd/dao/CandidatesDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import kr.smhrd.vo.*;
 
 public interface CandidatesDAO {
+  //인터페이스형태로 spring 구조 중 하나(솔직히 왜필요한지모르겠다만..)
 public List<CandidatesVO> candidatesList(SearchCriteria cri);
 public int candidatesInsert(CandidatesVO vo);
 public int candidatesDelete(int num);
