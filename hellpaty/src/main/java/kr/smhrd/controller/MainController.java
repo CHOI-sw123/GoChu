0e672d43a652e92e903d1acad36276d21292a5ed
gkstjd2605 <68883700+gkstjd2605@users.noreply.github.com>
2020-11-23 11:43:42 +0900

    Update MainController.java
    주석달기

--- hellpaty/src/main/java/kr/smhrd/controller/MainController.java
+++ hellpaty/src/main/java/kr/smhrd/controller/MainController.java
@@ -6,29 +6,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	@RequestMapping("/main.do")
+	//메인으로 들어가기
 	public String mainlogin() {
 		return "main";
 	}
 	
 	@RequestMapping("/membership.do")
+	//회원가입으로 들어가기
 	public String memberShip() {
 		return "member";
 	}
 	
 	@RequestMapping("/memberlogin.do")
+	//로그인으로 들어가기
 	public String memberLogin() {
 		return "memberLogin";
 	}
 
 	@RequestMapping("/resultDuty.do")
+	//직무별분석으로 들어가기
 	public String resultDuty() {
 		return "resultDuty";
 	}
 	@RequestMapping("/resultCandidate.do")
+	//지원자별분석으로 들어가기
 	public String resultCandidate() {
 		return "resultCandidate";
 	}
 	@RequestMapping("/resultDetails.do")
+	//분석하기 세부정보(따로구현된건없음)
 	public String resultDetails() {
 		return "resultDetails";
 	}
