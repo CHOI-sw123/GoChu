9ab47893e1b6fa4ec96c13310a38e76c87462e23
gkstjd2605 <68883700+gkstjd2605@users.noreply.github.com>
2020-11-23 12:01:00 +0900

    Update CandidatesDAOImpl.java

--- hellpaty/src/main/java/kr/smhrd/dao/CandidatesDAOImpl.java
+++ hellpaty/src/main/java/kr/smhrd/dao/CandidatesDAOImpl.java
@@ -16,12 +16,17 @@ import kr.smhrd.vo.*;
 public class CandidatesDAOImpl implements CandidatesDAO {
 
 	@Autowired
+	//세션 만들기위한거
 	private SqlSessionFactory sqlSessionFactory;
+	
+	
 	@Override
+	//리스트 출력
 	public List<CandidatesVO> candidatesList(SearchCriteria cri) {
 		SqlSession session = sqlSessionFactory.openSession();
 		List<CandidatesVO> list =null;
 		try {
+			//mapper에 있는 select를 사용해서 검색,페이징,리스트가 한번에 기능구현됨
 			list=session.selectList("cListpage", cri);
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -32,12 +37,14 @@ public class CandidatesDAOImpl implements CandidatesDAO {
 	}
 	
 	@Override
+	//페이징의 숫자를 표기하기위한 mapper연결
 	public int cdListCount(SearchCriteria cri) {
 		SqlSession session = sqlSessionFactory.openSession();
 		return session.selectOne("cdListCount",cri);
 	}
 	
 	@Override
+	//개별등록
 	public int candidatesInsert(CandidatesVO vo) {
 		SqlSession session = sqlSessionFactory.openSession();
 		try {
@@ -52,6 +59,7 @@ public class CandidatesDAOImpl implements CandidatesDAO {
 	}
 	
 	@Override
+	//삭제(기능구현안됨)
 	public int candidatesDelete(int num) {
 		SqlSession session = sqlSessionFactory.openSession();
 		try {
@@ -67,6 +75,7 @@ public class CandidatesDAOImpl implements CandidatesDAO {
 	
 
 	@Override
+	//엑셀 리스트 뽑기
 	public void cdInsertExcel(List<CandidatesVO> list) {
 		SqlSession session = sqlSessionFactory.openSession();
 		try {
